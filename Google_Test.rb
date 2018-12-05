35f4ebf3026dc6695977d87da203bbe480456662
JamesLittle1 <James.Little@makeitcheaper.com>
2018-12-05 11:36:49 +0000

    Tests fail when errors encountered

--- Google_Test.rb
+++ Google_Test.rb
@@ -6,8 +6,8 @@ session = Capybara::Session.new :selenium_firefox
 
 if (!pa11y_dashboard(session, '5c06557b244f7a3fcbad32a7'))
 puts 'fail'
-# exit -1
+exit -1
 else
 puts 'success'
-# exit 0
+exit 0
 end
