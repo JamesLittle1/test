ab27d7d8268ee18e6a367208bd897cf980fc6819
JamesLittle1 <James.Little@makeitcheaper.com>
2018-12-05 11:39:00 +0000

    Updating Ip for link

--- pa11y_dashboard.rb
+++ pa11y_dashboard.rb
@@ -7,7 +7,7 @@ notices = session.find(:id, 'notices')['innerHTML'].scan(/\d+/).first.to_i
 ignore = session.find(:id, 'ignore')['innerHTML'].scan(/\d+/).first.to_i
 
 puts "Errors: #{errors}\nWarnings: #{warnings}\nNotices: #{notices}\nIgnored Rules: #{ignore}"
-puts "Go to http://localhost:4000/#{id}?running for more details"
+puts "Go to http://10.1.0.43:4000/#{id}?running for more details"
 if(errors > 0)
 return false
 else
